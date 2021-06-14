e382e5b91ee339edb5809b1430c46a745fe49b81
lufuno <lufunnno3@gmail.com>
2021-06-14 06:59:03 +0200

    added task

---
+++ Task 0.2.js
@@ -0,0 +1,7 @@
+var sumResult_1 =  1 + 1 * 2;
+var sumResult_2 = (1 + 1) * 2;
+var sumResult_3 = 1 + ( 1 * 2 );
+var sumResult_4 = 1 + 1 * 2 / 2;
+var sumResult_5 = (1 + 1 * 2 ) /  2;
+
+console.log(sumResult_1 + sumResult_2 + sumResult_3 + sumResult_4 + sumResult_5);
