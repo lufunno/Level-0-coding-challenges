5cd8170ae864eddf60e070542004bbb9f3a77622
lufuno <lufunnno3@gmail.com>
2021-06-14 06:56:51 +0200

    Task added

---
+++ Task 0.1.js
@@ -0,0 +1,11 @@
+var number_1 = 0; 
+var number_2 = 1;
+
+console.log(number_1);
+console.log(number_2);
+
+number_1 = number_1 + 3;
+number_2 = number_2 + number_1;
+
+console.log(number_1);
+console.log(number_2);
