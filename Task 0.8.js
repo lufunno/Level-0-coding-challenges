7dfdfc370423a31c71a1fa0daca947fb386bf271
lufuno mbedzi <43001423+lufunno@users.noreply.github.com>
2021-06-16 14:39:59 +0200

    Update Task 0.8.js

--- Task 0.8.js
+++ Task 0.8.js
@@ -1,12 +1,23 @@
 function timeConverter(minutes){
-   var hour = 0;
-    while(minutes >= 60){
+    var hour = 0;
+    var min = minutes
+    while(minutes > 60){
         minutes -= 60;
         hour++;
     }
-    if (hour  == 1)
-    console.log(hour + " hour " + minutes + " minutes")
-    else{
-        console.log(hour + " hours " + minutes + " minutes")   
+    if (minutes == 60){
+        hour++;
+        if(hour > 1){
+            console.log(hour + " hours");
+        }
+        else{
+            console.log(hour + " hour");    
+        }
     }
+    else if(hour > 1 && minutes < 60){
+        console.log(hour + " hours " + minutes + " minutes")
+    }
+    else if (min < 60){
+        console.log(min + " minutes");
+    }   
 }
