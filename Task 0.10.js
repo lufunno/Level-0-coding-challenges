dd13d00ee6d0cc4185a7490c11acd7b80518216c
lufuno mbedzi <43001423+lufunno@users.noreply.github.com>
2021-09-16 15:37:30 +0200

    Update Task 0.10.js

--- Task 0.10.js
+++ Task 0.10.js
@@ -1,12 +1,12 @@
 function stringChar(str1,str2){
     
-    var charCompare =[];
+    let charCompare =[];
 
     for (var i=0; i<str2.length;i++){
         if (str1.includes (str2[i])){
             charCompare.push(str2[i]);
         }
     }
-    var char = charCompare.join();
+    let char = charCompare.join();
     console.log(char);
 }
