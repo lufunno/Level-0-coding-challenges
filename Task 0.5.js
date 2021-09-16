6da8fdb4d759f346be3f2451014b140460b226d4
lufuno mbedzi <43001423+lufunno@users.noreply.github.com>
2021-09-16 14:44:42 +0200

    Update Task 0.5.js

--- Task 0.5.js
+++ Task 0.5.js
@@ -1,5 +1,7 @@
-function triangleArea(sideA, sideB, sideC){
+function TriangleArea(sideA, sideB, sideC){
     let totalSide = (sideA + sideB + sideC) / 2;
     let area = Math.sqrt(totalSide * ((totalSide - sideA) * (totalSide - sideB) * (totalSide - sideC)));
     return area;
 }
+
+TriangleArea(10, 10, 10);
