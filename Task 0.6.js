f7042e5846e47cb77bde5cbf35781e0bb0a62fe1
lufuno mbedzi <43001423+lufunno@users.noreply.github.com>
2021-09-16 14:49:36 +0200

    Update Task 0.6.js

--- Task 0.6.js
+++ Task 0.6.js
@@ -1,19 +1,15 @@
-//first change the input to an array using '...'
-function maximumNumber(...input) {
+function MaximumNumber(...input) {
     var temp = 0;
-
-    //check the length of the array
     for (var i = 0; i < input.length; i++) {
-      for (var j = i; j < input.length; j++) {
-        //swap in ascending order
-        if (input[j] < input[i]) {
-          temp = input[j];
-          input[j] = input[i];
-          input[i] = temp;
+        for (var j = i; j < input.length; j++) {
+            if (input[j] < input[i]) {
+                temp = input[j];
+                input[j] = input[i];
+                input[i] = temp;
+            }
         }
-      }
     }
-    //getting the last element of our array
     var finalResult = input[input.length -1];
     return finalResult;
-  }
+}
+console.log(MaximumNumber(20, 30));
