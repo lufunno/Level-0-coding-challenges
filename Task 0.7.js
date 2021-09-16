29f99114a412f902ef20509dac0af58266016485
lufuno mbedzi <43001423+lufunno@users.noreply.github.com>
2021-09-16 14:55:22 +0200

    Update Task 0.7.js

--- Task 0.7.js
+++ Task 0.7.js
@@ -1,9 +1,11 @@
-function celsiusToFahrenheit(celsius){
+function CelsiusToFahrenheit(celsius){
    var fahrenheitResult = celsius * (9 / 5) + 32;
    return fahrenheitResult + ' \xB0F';   
 }
 
-function fahrenheiToCelsius(fahrenheit){
+function FahrenheiToCelsius(fahrenheit){
     var celciusResult = (fahrenheit - 32) * 5 / 9;
     return celciusResult + ' \xB0C';
 }
+console.log(CelsiusToFahrenheit(30));
+console.log(FahrenheiToCelsius(30));
